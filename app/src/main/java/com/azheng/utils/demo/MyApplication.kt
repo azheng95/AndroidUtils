3e584212a5b533193f074c0b770f9d1b692db2c5
azheng <weibangzheng@workersaas.com>
2025-03-26 18:28:39 +0800

    修改注释

--- app/src/main/java/com/azheng/utils/demo/MyApplication.kt
+++ app/src/main/java/com/azheng/utils/demo/MyApplication.kt
@@ -4,12 +4,9 @@ import android.app.Application
 import com.azheng.androidutils.Utils
 
 class MyApplication : Application() {
-    
     override fun onCreate() {
         super.onCreate()
-        
-        // 初始化AppUtils
+        // 初始化Utils
         Utils.init(this)
-
     }
 }
